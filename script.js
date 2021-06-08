d3bcebed8f25139fb8a13f6e0de2a971b2111c47
TrentHodgins <>
2021-06-08 16:58:39 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-//This line connects the button to a function//. 
-document.getElementById('button').addEventListener('click',randomizer)
+// This line connects the button to a function//.
+document.getElementById('button').addEventListener('click', randomizer)
 let randomNumber = 0
 let userGuess = 0
 
-function randomizer() {
-  //get number typed in by user and convert to an integer//
+function randomizer () {
+  // get number typed in by user and convert to an integer//
   userGuess = document.getElementById('input').value
   userGuess = parseInt(userGuess)
 
-  //generate random number between 1 and 6
-  randomNumber = (Math .random() * 6) + 1
+  // generate random number between 1 and 6
+  randomNumber = (Math.random() * 6) + 1
   randomNumber = parseInt(randomNumber)
 
-  //compare user's guess with random numer
-  if (userGuess == randomNumber) {
-  document.getElementById('answer').innerHTML = "You Win!"
+  // compare user's guess with random numer
+  if (userGuess === randomNumber) {
+    document.getElementById('answer').innerHTML = 'You Win!'
   }
 }
